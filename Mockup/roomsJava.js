1a4539d5d9c4aa7a67a1d81493dc0dab99703e02
irhadLjubcic1 <irhad.ljubcic@gmail.com>
2016-02-11 22:08:08 +0100

    Pogledajte :)

---
+++ Mockup/roomsJava.js
@@ -0,0 +1,26 @@
+ function onBtnClick () {
+     document.getElementById("thisWeek").style.color="#0000ff";
+    
+}
+ function test() {
+     document.getElementById("test1").style.backgroundColor = "#3475CD";
+ }
+ function setDate() {
+     var d = new Date();
+     if (d.getDay() == 1){
+         d = "Monday";
+     }
+    else if (d.getDay() == 2) {
+         d= "Tuesday";
+     }
+    else if (d.getDay() == 3) {
+         d = "Wednesday";
+     }
+    else if (d.getDay() == 4) {
+         d = "Thursday";
+     }
+     else {
+         d = "Friday";
+     }
+     document.getElementById("date").innerHTML = d;
+ }
