976f979c4e27b09316096da3df8b72893639ec21
nedim-cimpo <nedim@gigischool.com>
2016-02-13 00:39:47 +0100

    test

---
+++ Database/ProjectSkill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class ProjectSkill
+    {
+        public int Id { get; set; }
+        public int ToolId { get; set; }
+        public int TeamId { get; set; }
+        public int Level { get; set; }
+        sdfdf
+    }
+}
