88aaaaf53cbec2c2bad509af3e06f3c951149e78
Dalila Avdukic <Dalila Avdukic>
2016-02-15 16:20:29 +0100

    add SkillsLibrary entities to DbContext

--- Database/EmployeeSkill.cs
+++ Database/EmployeeSkill.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Database
 {
-    class EmployeeSkill
+    public class EmployeeSkill
     {
         public int Id { get; set; }
         public int ToolId { get; set; }
--- Database/SkillCategory.cs
+++ Database/SkillCategory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Database
 {
-    class SkillCategory
+    public class SkillCategory
     {
         public int Id { get; set; }
         public string Name { get; set; }
