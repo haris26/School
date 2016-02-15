1327127382e926e6b21951e3a4b982db76b2a873
nedim-cimpo <nedim@gigischool.com>
2016-02-15 12:09:06 +0100

    za dalilu

---
+++ Database/EmployeeSkill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class EmployeeSkill
+    {
+        public int Id { get; set; }
+        public int ToolId { get; set; }
+        public int EmployeeId { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+    }
+}
