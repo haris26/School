67f389a3638dcfc3b1e248ad24a1cafb029b816e
Dzanan Gvozden <dzanan@gigischool.com>
2016-02-16 16:34:35 +0100

    modifying database

--- Database/Detail.cs
+++ Database/Detail.cs
@@ -9,11 +9,10 @@ namespace Database
     public class Detail
     {
         public int Id { get; set; }
-        public int TimeCategoryId { get; set; }
-        public int CalendarId { get; set; }
-        public int TeamId { get; set; }
+        public virtual Calendar Calendar { get; set; }
         public double WorkTime { get; set; }
         public double BillTime { get; set; }
         public string Description { get; set; }
+        public virtual Team Team { get; set; }
     }
 }
