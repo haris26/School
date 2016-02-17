4f07c89c4c0b21bb290bf76ff70a771c3927f508
Martina Vistica <martina@gigischool.com>
2016-02-17 10:48:13 +0100

    cleaning

--- Database/CharacteristicName.cs
+++ Database/CharacteristicName.cs
@@ -9,6 +9,7 @@ namespace Database
     // Name of characteristics for specific category 
     public class CharacteristicName
     {
+        public int Id { get; set; }
         public virtual ResourceCategory ResourceCategory { get; set; }      //Navigation to the resource category
         public string Name { get; set; }                                    // Name of characteristics
     }
