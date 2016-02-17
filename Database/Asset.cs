ccb57a12fb0bd4f1b993f8b1281e8476c7ef911b
Irma12 <isolakovic1@etf.unsa.ba>
2016-02-17 15:21:25 +0100

    commit1020

--- Database/Asset.cs
+++ Database/Asset.cs
@@ -1,5 +1,4 @@
 ﻿// PROCUREMENT SYSTEM
-=======
 ﻿using Database;
 using System;
 using System.Collections.Generic;
@@ -9,19 +8,20 @@ using System.Threading.Tasks;
 
 namespace Database
 {
-//  Basic assets data
+    //  Basic assets data
     public class Asset
-    {       
+    {
         // public int AssetId { get; set; } instead of classId in all our projects we will use just Id (convention)
         public int Id { get; set; }
-        
+
         public string Description { get; set; }     // asset description [name]
         public string Vendor { get; set; }          // vendor - so far just description - maybe separate class in the future
         public double Price { get; set; }           // price
-        
+
         // public string EmployeeID { get; set; } we will use navigation to person instead of simple foreign key
         public Person User { get; set; }            // person who use particular asset 
-        
+
         // public enum Status { Active=1,Coming_soon=2, Out_of_order=3 } we will put status to enumerators
         public AssetStatus Status { get; set; }
+    }
 }
