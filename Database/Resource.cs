22f569a282628c3f1d03b534a603a86d8e3b0e75
Martina Vistica <martina@gigischool.com>
2016-02-17 16:54:12 +0100

    reading DataSeed from Excel file

--- Database/Resource.cs
+++ Database/Resource.cs
@@ -4,9 +4,9 @@ namespace Database
 //  Resources
     public class Resource
     {
-        public int Id { get; set; }             // Identity[1]
-        public string Name { get; set; }        // Name
-
-        public virtual ResourceCategory  ResourceCategory { get; set;}     // Navigation to the resource category
+        public int Id { get; set; }                                         // Identity[1]
+        public string Name { get; set; }                                    // Name
+        public ReservationStatus Status { get; set; }                       // Reservation status of resource
+        public virtual ResourceCategory  ResourceCategory { get; set;}      // Navigation to the resource category
     }
 }
