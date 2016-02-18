8411fe7c471dd4020d13f7f1b1264f984c597199
nedim-cimpo <nedim@gigischool.com>
2016-02-18 15:26:18 +0100

    merging seed

--- DataSeed/Program - Alfa.cs
+++ DataSeed/Program - Alfa.cs
@@ -1,102 +1,119 @@
-﻿//using Database;
-//using System;
-//using System.Data;
-//using System.Data.OleDb;
+﻿using Database;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
 
-//namespace DataSeed
-//{
-//    class AlfaSeed
-//    {
-//        static string sourceData = @"C:\Projects\school\omega.xls";
-//        static SchoolContext context = new SchoolContext();
+namespace DataSeed
+{
+    class AlfaSeed
+    {
+        static string sourceData = @"C:\Projects\school\alfa.xls";
+        static SchoolContext context = new SchoolContext();
 
-//        static void Main(string[] args)
-//        {
-//            getSkillCategories();
-//            getTeams();
-//            getPeople();
-//        }
+        static void Main(string[] args)
+        {
+            getEducation();
+            getSkillCategory();
+            getTool();
+        }
 
-//        static void getSkillCategories()
-//        {
-//            //Console.Write("ROLES: ");
-//            //DataTable rawData = OpenExcel(sourceData, "Roles");
-//            //int N = 0;
-//            //foreach (DataRow row in rawData.Rows)
-//            //{
-//            //    Role role = new Role()
-//            //    {
-//            //        Name = row.ItemArray.GetValue(1).ToString(),
-//            //    };
-//            //    N++;
-//            //    context.Roles.Add(role);
-//            //}
-//            //context.SaveChanges();
-//            //Console.WriteLine(N);
-//        }
+        static void getEducation()
+        {
+            Console.Write("EDUCATION: ");
+            DataTable rawData = OpenExcel(sourceData, "Education");
+            int N = 0;
+            foreach (DataRow row in rawData.Rows)
+            {
+                Education education = new Education()
+                {
+                    Name = getString(row, 0),
+                    Type = (EducationType)getInteger(row, 1),
+                    Reference = getString(row, 2)
+                };
+                N++;
+                context.Educations.Add(education);
+            }
+            context.SaveChanges();
+            Console.WriteLine(N);
+        }
 
-//        static void getTeams()
-//        {
-//            //Console.Write("TEAMS: ");
-//            //DataTable rawData = OpenExcel(sourceData, "Teams");
-//            //int N = 0;
-//            //foreach (DataRow row in rawData.Rows)
-//            //{
-//            //    Team team = new Team()
-//            //    {
-//            //        Name = row.ItemArray.GetValue(1).ToString(),
-//            //        Description = row.ItemArray.GetValue(2).ToString(),
-//            //        Type = (ProjectType)Convert.ToInt32(row.ItemArray.GetValue(3).ToString())
-//            //    };
-//            //    int type = Convert.ToInt32(row.ItemArray.GetValue(3).ToString());
-//            //    switch (type)
-//            //    {
-//            //        case 1: { team.Type = ProjectType.External; break; }
-//            //        case 2: { team.Type = ProjectType.Internal; break; }
-//            //        default: { team.Type = ProjectType.Absence; break; }
-//            //    }
-//            //    N++;
-//            //    context.Teams.Add(team);
-//            //}
-//            //context.SaveChanges();
-//            //Console.WriteLine(N);
-//        }
+        static void getSkillCategory()
+        {
+            Console.Write("SKILL CATEGORY: ");
+            DataTable rawData = OpenExcel(sourceData, "SkillCategory");
+            int N = 0;
+            foreach (DataRow row in rawData.Rows)
+            {
+                SkillCategory skillcategory = new SkillCategory()
+                {
+                    Name = getString(row, 0),
+                };
+                N++;
+                context.SkillCategories.Add(skillcategory);
+            }
+            context.SaveChanges();
+            Console.WriteLine(N);
+        }
 
-//        static void getPeople()
-//        {
-//            //Console.Write("PEOPLE: ");
-//            //DataTable rawData = OpenExcel(sourceData, "People");
-//            //int N = 0;
-//            //foreach (DataRow row in rawData.Rows)
-//            //{
-//            //    Person person = new Person()
-//            //    {
-//            //        FirstName = row.ItemArray.GetValue(1).ToString(),
-//            //        LastName = row.ItemArray.GetValue(2).ToString()
-//            //    };
-//            //    N++;
-//            //    context.People.Add(person);
-//            //}
-//            //context.SaveChanges();
-//            //Console.WriteLine(N);
-//        }
+        static void getTool()
+        {
+            Console.Write("TOOLS: ");
+            DataTable rawData = OpenExcel(sourceData, "Tool");
+            int N = 0;
+            foreach (DataRow row in rawData.Rows)
+            {
+                string categoryName = getString(row, 0);
+                SkillCategory category = context.SkillCategories.Where(x => x.Name == categoryName).FirstOrDefault();
+
+                Tool tool = new Tool()
+                {
+                    Name = getString(row, 0),
+                    Category = category
+                };
+                N++;
+                context.Tools.Add(tool);
+            }
+            context.SaveChanges();
+            Console.WriteLine(N);
+        }
 
 
-//        static DataTable OpenExcel(string path, string sheet)
-//        {
-//            var cs = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0", path);
-//            OleDbConnection conn = new OleDbConnection(cs);
-//            conn.Open();
+        static DataTable OpenExcel(string path, string sheet)
+        {
+            var cs = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0", path);
+            OleDbConnection conn = new OleDbConnection(cs);
+            conn.Open();
 
-//            OleDbCommand cmd = new OleDbCommand(string.Format("SELECT * FROM [{0}$]", sheet), conn);
-//            OleDbDataAdapter da = new OleDbDataAdapter();
-//            da.SelectCommand = cmd;
+            OleDbCommand cmd = new OleDbCommand(string.Format("SELECT * FROM [{0}$]", sheet), conn);
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            da.SelectCommand = cmd;
 
-//            System.Data.DataTable dt = new System.Data.DataTable();
-//            da.Fill(dt);
-//            conn.Close();
+            System.Data.DataTable dt = new System.Data.DataTable();
+            da.Fill(dt);
+            conn.Close();
 
-//            return dt;
-//        }
-//    }
-//}
+            return dt;
+        }
+
+        static string getString(DataRow row, int index)
+        {
+            return row.ItemArray.GetValue(index).ToString();
+        }
+
+        static int getInteger(DataRow row, int index)
+        {
+            return Convert.ToInt32(row.ItemArray.GetValue(index).ToString());
+        }
+
+        static bool getBool(DataRow row, int index)
+        {
+            return (row.ItemArray.GetValue(index).ToString().ToLower() == "yes");
+        }
+
+        static DateTime getDate(DataRow row, int index)
+        {
+            return Convert.ToDateTime(row.ItemArray.GetValue(index).ToString());
+        }
+    }
+}
