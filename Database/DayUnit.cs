5ebdfb4bb68121428e62f8c2d15e5988dae01ddf
Dzanan Gvozden <dzanan@gigischool.com>
2016-02-22 20:21:02 +0100

    merging

--- Database/DayUnit.cs
+++ Database/DayUnit.cs
@@ -4,13 +4,9 @@ namespace Database
 {
     public class DayUnit: Repository<Day>
     {
-<<<<<<< HEAD
-        
 
-        public DayUnit(SchoolContext context): base(context) { }
-=======
         public DayUnit(SchoolContext context) : base(context) { }
->>>>>>> e1cd4b7cd8f69542dd1ae3ce1cd6cc30dd0605ea
+
 
         public override void Insert(Day entity)
         {
