0ad60990397f25ee1d88dda27a41485da8b4c1fa
haris26 <haris.brackovic91@gmail.com>
2016-02-22 16:20:08 +0100

    MVC Haris

---
+++ ReservationSystem/ReservationSystemMVC/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservationSystemMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return "Gdje ste rajaaaa";
+        }
+        public string Show()
+        {
+            return "Another string from the same controleer";
+        }
+        public string Look(string id)
+        {
+            return "You just sent: " + id;
+        }
+        public ActionResult Get(string id = "X")
+        {
+            if (id == "X")
+            {
+                ViewBag.Title = "ReservationSystem";
+                return View();
+            }
+            else
+            {
+                return Content("Proslijedise mi: " + id);
+            }
+        }
+        public ActionResult People()
+        {
+            Repository<Person> people = new Repository<Person>(new SchoolContext());
+            return View(people.Get().ToList());
+        }
+        public ActionResult Person(int id)
+        {
+            Repository<Person> people = new Repository<Person>(new SchoolContext());
+            Person person = people.Get(id);
+            //ViewBag.Person = person;
+            //ViewData["Person"] = person;
+            return View(person);
+        }
+    }
+}
