346df3947244f6fef62a91a34e9e012e5ef76f9b
elma-ramusovic <elma@gigischool.com>
2016-02-23 16:44:42 +0100

    timetracking

---
+++ TimeTracking/Models/TeamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracking.Models
+{
+    public class TeamModel
+    {
+        public TeamModel(){
+            Members = new List<string>();
+            }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<string> Members { get; set; }
+    }
+}
