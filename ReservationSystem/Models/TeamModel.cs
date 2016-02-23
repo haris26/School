f20b6fdff514ad4949214adfeac897b094c13f60
haris26 <haris.brackovic91@gmail.com>
2016-02-23 16:47:44 +0100

    Haris-Beta

---
+++ ReservationSystem/Models/TeamModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReservationSystem.Models
+{
+    public class TeamModel
+    {
+        public TeamModel()
+        {
+            Members = new List<string>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<string> Members { get; set; }
+    }
+}
