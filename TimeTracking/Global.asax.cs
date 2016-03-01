b6803d37ab7cefc8e2230fbac4c9f766bc487633
Dzanan Gvozden <dzanan@gigischool.com>
2016-03-01 17:42:12 +0100

    merge

--- TimeTracking/Global.asax.cs
+++ TimeTracking/Global.asax.cs
@@ -5,8 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TimeTracking;
 
-namespace TimeTracking
+namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
