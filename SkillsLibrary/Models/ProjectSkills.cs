eb904fb91d2ec5edf67e04550d82395d072b2bc6
dalila-avdukic <dalila@gigischool.com>
2016-03-02 16:07:31 +0100

    master-detail for project skills

---
+++ SkillsLibrary/Models/ProjectSkills.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Database;
+
+namespace SkillsLibrary.Models
+{
+    public class ProjectSkills
+    {
+        public ProjectSkills()
+        {
+            Skills = new List<ProjectSkillModel>();
+        }
+
+        public Team Team { get; set; }
+        public IList<ProjectSkillModel> Skills { get; set; }
+    }
+}
