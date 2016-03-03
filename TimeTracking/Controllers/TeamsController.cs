bc842a3a1fdb7a674911dfabe86728008241dd01
Dzanan Gvozden <dzanan@gigischool.com>
2016-03-03 10:48:39 +0100

    merge

--- TimeTracking/Controllers/TeamsController.cs
+++ TimeTracking/Controllers/TeamsController.cs
@@ -1,85 +1,156 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.Entity;
-//using System.Linq;
-//using System.Net;
-//using System.Web;
-//using System.Web.Mvc;
-//using Database;
-//using TimeTracking.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Database;
+using TimeTracking.Models;
 
+namespace TimeTracking.Controllers
+{
+    public class TeamsController : BaseController
+    {
+        private Repository<Team> teams = new Repository<Team>(new SchoolContext());
 
-//namespace TimeTracking.Controllers
-//{
-//    public class TeamsController : Controller
-//    {
-//        private Repository<Team> teams = new Repository<Team>(new SchoolContext());
+        public ActionResult Index()
+        {
+            return View(teams.Get().ToList().Select(x => Factory.Create(x)).ToList());
+        }
 
+        public ActionResult Details(int id)
+        {
+            return View(Factory.Create(teams.Get(id)));
+        }
 
-//        public ActionResult Index()
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                teams.Insert(team);
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(teams.Get(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                teams.Update(team, team.Id);
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(teams.Get(id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            teams.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+//        public ActionResult ProjectSkills(int id)
 //        {
-//            List<TeamModel> teamList = new List<TeamModel>();
-//            var teamsCol = teams.Get().ToList();
-//            foreach (var team in teamsCol)
+//            ProjectSkills model = new ProjectSkills();
+//            model.Team = new Repository<Team>(Context).Get(id);
+//            model.Skills = new ProjectSkillUnit(Context)
+//                           .Get().Where(x => x.Team.Id == id).ToList()
+//                           .Select(x => Factory.Create(x)).ToList();
+//            return View(model);
+//        }
+
+
+//        public ActionResult SkillCreate(int id)   // id = Team.Id
+//        {
+//            FillBag();
+//            Team team = new Repository<Team>(Context).Get(id);
+//            return View(new ProjectSkillModel()
 //            {
-//                TeamModel model = factory.Create(team);
-//                teamList.Add(model);
-//            }
-//            return View(teamList);
-//            return View(teams.Get().ToList().Select(x => factory.Create(x)).ToList());
+//                Project = team.Id,
+//                ProjectName = team.Description,
+//                TeamName = team.Name
+//            });
 //        }
 
-//        public ActionResult Details(int id)
+//        public ActionResult SkillEdit(int id)     // id = ProjectSkill.Id
 //        {
-//            return View(factory.Create(teams.Get(id)));
-//        }
-
-//        public ActionResult Create()
-//        {
-//            return View();
+//            FillBag();
+//            return View(Factory.Create(new ProjectSkillUnit(Context).Get(id)));
 //        }
 
 //        [HttpPost]
 //        [ValidateAntiForgeryToken]
-//        public ActionResult Create(Team team)
+//        public ActionResult SkillCreate(ProjectSkillModel model)
 //        {
 //            if (ModelState.IsValid)
 //            {
-//                teams.Insert(team);
-//                return RedirectToAction("Index");
+//                ProjectSkill projSkill = Parser.Create(model);
+//                new ProjectSkillUnit(Context).Insert(projSkill);
+//                return RedirectToAction("ProjectSkills/" + model.Project);
 //            }
-//            return View(team);
-//        }
-
-//        public ActionResult Edit(int id)
-//        {
-//            return View(teams.Get(id));
+//            return View(model);
 //        }
 
 //        [HttpPost]
 //        [ValidateAntiForgeryToken]
-//        public ActionResult Edit(Team team)
+//        public ActionResult SkillEdit(ProjectSkillModel model)
 //        {
 //            if (ModelState.IsValid)
 //            {
-//                teams.Update(team, team.Id);
-//                return RedirectToAction("Index");
+//                ProjectSkill projSkill = Parser.Create(model);
+//                new ProjectSkillUnit(Context).Update(projSkill, projSkill.Id);
+//                return RedirectToAction("ProjectSkills/" + model.Project);
 //            }
-//            return View(team);
+//            return View(model);
 //        }
 
-//        public ActionResult Delete(int id)
+//        // GET: People/SkillDelete/5
+//        public ActionResult SkillDelete(int id)
 //        {
-//            return View(teams.Get(id));
+//            return View(Factory.Create(new ProjectSkillUnit(Context).Get(id)));
 //        }
 
-//        [HttpPost, ActionName("Delete")]
+//        // POST: People/SkillDelete/5
+//        [HttpPost, ActionName("SkillDelete")]
 //        [ValidateAntiForgeryToken]
-//        public ActionResult DeleteConfirmed(int id)
+//        public ActionResult SkillDeleteConfirmed(int id)
 //        {
-//            teams.Delete(id);
-//            return RedirectToAction("Index");
+//            ProjectSkillUnit projSkills = new ProjectSkillUnit(Context);
+//            int project = projSkills.Get(id).Team.Id;
+//            projSkills.Delete(id);
+//            return RedirectToAction("ProjectSkills/" + project);
+//        }
+
+
+
+//        void FillBag()
+//        {
+//            ViewBag.ToolsList = new SelectList(new Repository<Tool>(Context).Get().ToList(), "Id", "Name");
 //        }
 //    }
 //}
