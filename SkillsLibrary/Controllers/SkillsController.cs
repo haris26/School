3e990fd4bbc56389cbaca518c71e07d28b66f8c8
dalila-avdukic <dalila@gigischool.com>
2016-03-08 16:25:49 +0100

    merge dalila skillscontroller

---
+++ SkillsLibrary/Controllers/SkillsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Database;
+
+namespace SkillsLibrary.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Skills
+        public ActionResult Index()
+        {
+            return View(db.Tools.ToList());
+        }
+
+        // GET: Skills/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tool tool = db.Tools.Find(id);
+            if (tool == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tool);
+        }
+
+        // GET: Skills/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Tool tool)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tools.Add(tool);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tool);
+        }
+
+        // GET: Skills/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tool tool = db.Tools.Find(id);
+            if (tool == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tool);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Tool tool)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tool).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tool);
+        }
+
+        // GET: Skills/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tool tool = db.Tools.Find(id);
+            if (tool == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tool);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tool tool = db.Tools.Find(id);
+            db.Tools.Remove(tool);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
