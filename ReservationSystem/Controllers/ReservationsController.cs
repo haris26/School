22410155a10a68a92dc3977b89556cf0725da0a3
Martina Vistica <martina@gigischool.com>
2016-03-09 11:38:31 +0100

    prepared controller/model/view for room reservation

--- ReservationSystem/Controllers/ReservationsController.cs
+++ ReservationSystem/Controllers/ReservationsController.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Database;
+using ReservationSystem.Models;
 
 namespace ReservationSystem.Controllers
 {
-    public class ReservationsController : Controller
+    public class ReservationsController : BaseController
     {
         // GET: Reservations
         public ActionResult Index()
         {
+            ReservationModel model = new ReservationModel();
+            model.Events = new EventUnit(Context).Get().ToList().Select(x => Factory.Create(x)).ToList();
+            model.Resources = new ResourceUnit(Context).Get().ToList().Select(x => Factory.Create(x)).ToList();
+            model.ResourceCharacteristics = new Repository<Characteristic>(Context).Get().ToList();
             return View();
         }
 
         public ActionResult Rooms()
         {
-            return View();
+            ReservationModel model = new ReservationModel();
+            model.Events = new EventUnit(Context).Get().ToList().Where(x => x.Resource.ResourceCategory.CategoryName == "Room").Select(x => Factory.Create(x)).ToList();
+            model.Resources = new ResourceUnit(Context).Get().ToList().Where(x => x.ResourceCategory.CategoryName == "Room").Select(x => Factory.Create(x)).ToList();
+            model.ResourceCharacteristics = new Repository<Characteristic>(Context).Get().Where(x => x.Resource.ResourceCategory.CategoryName == "Room").ToList();
+            return View(model);
         }
 
         public ActionResult Devices()
         {
-            return View();
+            ReservationModel model = new ReservationModel();
+            model.Events = new EventUnit(Context).Get().ToList().Where(x => x.Resource.ResourceCategory.CategoryName == "Device").Select(x => Factory.Create(x)).ToList();
+           
+            model.Resources = new ResourceUnit(Context).Get().Where(x => x.ResourceCategory.CategoryName == "Device").ToList()
+                .Select(x => Factory.Create(x)).ToList();
+            model.ResourceCharacteristics = new Repository<Characteristic>(Context).Get().ToList().Where(x => x.Resource.ResourceCategory.CategoryName == "Device").ToList();
+
+            //ViewBag.ResourceList = new SelectList(new ResourceUnit(Context).Get().ToList().Where(x => x.ResourceCategory.CategoryName == "Device" ), "Id", "Name");
+
+            return View(model);
         }
     }
 }
---
+++ ReservationSystem/Models/ReservationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Database;
+
+namespace ReservationSystem.Models
+{
+    public class ReservationModel
+    {
+        public ReservationModel()
+        {
+            Resources = new List<ResourceModel>();
+            ResourceCharacteristics = new List<Characteristic>();
+            Events = new List<EventModel>();
+        }
+        public int Id { get; set; }
+        public IList<ResourceModel> Resources { get; set; }
+        public IList<Characteristic> ResourceCharacteristics { get; set; }
+        public IList<EventModel> Events { get; set; }
+    }
+}
