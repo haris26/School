dc0930815ef48f98062bfe8cde54640291138d2a
haris26 <haris.brackovic91@gmail.com>
2016-03-16 15:15:22 +0100

    Reservation controller and context

---
+++ WebAPI/Models/EntityParser.cs
@@ -0,0 +1,30 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class EntityParser
+    {
+        private SchoolContext context;
+
+        public EntityParser(SchoolContext _context)
+        {
+            context = _context;
+        }
+       
+        public Resource Create(ResourceModel model)
+
+        {
+            return new Resource()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Status = model.Status,
+                ResourceCategory = context.ResourceCategories.Find(model.ResourceCategory)
+            };
+        }
+    }
+}
