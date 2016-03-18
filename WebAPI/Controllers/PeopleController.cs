147fb38f4b556f2cde4733345d07909031d597b5
AlenBumbulovic <AlenBumbulovic>
2016-03-18 14:37:52 +0100

    people controller

--- WebAPI/Controllers/PeopleController.cs
+++ WebAPI/Controllers/PeopleController.cs
@@ -1,13 +1,102 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi.Controllers;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
-    public class PeopleController : ApiController
+    public class PeopleController : BaseController<Person>
     {
+        public PeopleController(Repository<Person> depo) : base(depo) { }
+
+
+        public Object GetAll(int page = 0)
+        {
+            int PageSize = 5;
+            var query =
+               Repository.Get()
+                   .OrderBy(x => x.FirstName)
+                   .ThenBy(x => x.LastName)
+                   .ToList();
+
+            int TotalPages = (int)Math.Ceiling((double)query.Count() / PageSize);
+
+            IList<PeopleModel> people =
+                query.Skip(PageSize * page).Take(PageSize).ToList().Select(x => Factory.Create(x)).ToList();
+
+            return new
+            {
+                pageSize = PageSize,
+                currentPage = page,
+                pageCount = TotalPages,
+                allPeople = people
+            };
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Person person = Repository.Get(id);
+                if (person == null)
+                {
+                    return NotFound();
+
+                }
+                else
+
+                    return Ok(Factory.Create(person));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult Post(PeopleModel model)
+        {
+            try
+            {
+                Repository.Insert(Parser.Create(model, Repository.BaseContext()));
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult Put(int id, PeopleModel model)
+        {
+            try
+            {
+                Repository.Update(Parser.Create(model, Repository.BaseContext()), model.Id);
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                Repository.Delete(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
     }
 }
