e60f9de3b7f3d775de2c6ab8d196e883b9e6f497
Irma <irmas@gigischool.com>
2016-03-22 14:34:32 +0100

    Login and authentication

--- WebAPI/Controllers/LoginController.cs
+++ WebAPI/Controllers/LoginController.cs
@@ -1,9 +1,19 @@
-﻿﻿using Database;
+﻿using Database;
+using System;
+using System.Security.Principal;
+using System.Threading;
 using System.Web.Http;
 using WebApi.Helpers;
+using WebMatrix.WebData;
 
 namespace WebApi.Controllers
 {
+    public class UserModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
     public class LoginController : ApiController
     {
         Repository<Person> people = new Repository<Person>(new SchoolContext());
@@ -15,6 +25,7 @@ namespace WebApi.Controllers
                 Person person = people.Get(id);
                 if (person != null)
                 {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(person.FirstName), null);
                     AppGlobals.currentUser = person;
                     return Ok(person.FirstName);
                 }
@@ -28,5 +39,27 @@ namespace WebApi.Controllers
                 return BadRequest();
             }
         }
+
+        public IHttpActionResult Get()
+        {
+            if (!WebSecurity.Initialized) WebSecurity.InitializeDatabaseConnection("School", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            WebSecurity.Logout();
+            return Ok();
+        }
+
+        public IHttpActionResult Post(UserModel user)
+        {
+            try
+            {
+                if (!WebSecurity.Initialized) WebSecurity.InitializeDatabaseConnection("School", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                WebSecurity.CreateUserAndAccount(user.username, user.password, false);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
