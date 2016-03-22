0982cfebd07d7fb54a262664be54029cc7e60499
Nedim Čimpo <Nedim impo>
2016-03-22 09:13:38 +0100

    teamsummary

---
+++ WebAPI/Controllers/TeamSummariesController.cs
@@ -0,0 +1,42 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Helpers;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class TeamSummariesController : BaseController<Team>
+    {
+        public TeamSummariesController(Repository<Team> depo) : base(depo)
+        { }
+
+        public IList<TeamModel> Get()
+        {
+            return Repository.Get().ToList().Select(x => Factory.Create(x)).ToList();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Team team;
+                team = Repository.Get(id);
+                if (team == null)
+                {
+                    return NotFound();
+                }
+                return Ok(TeamSummary.Create(team));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ WebAPI/Helpers/TeamSummary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+using Database;
+
+namespace WebAPI.Helpers
+{
+    public class TeamSummary
+    {
+        public static TeamSummaryModel Create(Team team)
+        {
+            TeamSummaryModel teamSummary = new TeamSummaryModel()
+            {
+                Name = team.Name,
+                PeopleInTeam = team.Members.Count()
+            };
+
+            teamSummary.RequiredSkills = team.ProjectSkills.ToList().Select(x => CreateRequiredSkill(x)).ToList();
+
+            teamSummary.Skills = 
+
+            return teamSummary;
+        }
+
+        public static RequiredSkill CreateRequiredSkill(ProjectSkill projectSkill)
+        {
+            return new RequiredSkill()
+            {
+                Skill = projectSkill.Tool.Name,
+                Level = (int)projectSkill.Level
+            };
+        }
+
+        public static TeamSkillSummary CreateTeamSkillSummary(IGrouping<int, ProjectSkill> x)
+        {
+            return new TeamSkillSummary()
+            {
+                AverageLevel = 
+            };
+        }
+
+    }
+}
