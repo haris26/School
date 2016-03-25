e5d6bf08fe88a01a4275a1316cac791ff0a11042
lejla-dzeko <lejla@gigischool.com>
2016-03-25 16:35:48 +0100

    business days in a month

--- WebAPI/Helpers/Dashboard.cs
+++ WebAPI/Helpers/Dashboard.cs
@@ -21,8 +21,15 @@ namespace WebAPI.Helpers
                 Name = person.FullName
 
             };
+
             DateTime dd = DateTime.Now; //(year: 2016, month: 3, day: 1);
             int bd = DateTime.DaysInMonth(dd.Year, dd.Month);
+
+
+            var weekends = new DayOfWeek[] { DayOfWeek.Saturday, DayOfWeek.Sunday };
+         
+            IEnumerable<int> businessDaysInMonth = Enumerable.Range(1, bd)
+                                                   .Where(d => !weekends.Contains(new DateTime(dd.Year, dd.Month, d).DayOfWeek));
 
             var days =
                person.Days.GroupBy(x => x.Person.FirstName)
@@ -31,8 +38,11 @@ namespace WebAPI.Helpers
             foreach (var d in days)
             {
 
-                dashboard.CountEmpty = bd - d.days;
+                dashboard.CountEmpty = businessDaysInMonth.Count() - d.days;
             }
+
+
+
             var details = person.Days.SelectMany(x => x.Details).GroupBy(x => x.Team.Name).Select(x => new { team = x.Key, time = x.Sum(y => y.WorkTime) }).ToList();
             foreach (var detail in details)
             {
