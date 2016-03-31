e2c4c303babbbeb47322c59dd57cca688d8a9fcf
irhad-ljubicic <irhad@gigischool.com>
2016-03-31 16:21:58 +0200

    changing UserReservation

--- WebAPI/Helpers/UserReservations.cs
+++ WebAPI/Helpers/UserReservations.cs
@@ -19,32 +19,45 @@ namespace WebAPI.Helpers
             };
 
             var reservations = new EventUnit(context).Get().ToList().Where(x => (x.User.FullName == AppGlobals.currentUser.FullName));
+
             foreach (var reservation in reservations)
             {
                 if (reservation.EventStart == System.DateTime.Today)
                 {
-                    model.TodayReservations.Add(new ReservationModel()
+                    model.TodayReservations.Add(new EventModel()
                     {
                         Id = reservation.Id,
-                        ResourceName = reservation.Resource.Name,
+                        EventTitle = reservation.EventTitle,
                         StartDate = reservation.EventStart,
-                        EndDate = reservation.EventEnd
+                        EndDate = reservation.EventEnd,
+                        Person = AppGlobals.currentUser.Id,
+                        PersonName = AppGlobals.currentUser.FullName,
+                        Resource = reservation.Resource.Id,
+                        ResourceName = reservation.Resource.Name,
+                        Category = reservation.Resource.ResourceCategory.Id,
+                        CategoryName = reservation.Resource.ResourceCategory.CategoryName,
                     });
                 }
                 else if (reservation.EventStart > System.DateTime.Now)
                 {
-                    model.UpcomingReservations.Add(new ReservationModel()
+                    model.UpcomingReservations.Add(new EventModel()
                     {
                         Id = reservation.Id,
-                        ResourceName = reservation.Resource.Name,
+                        EventTitle = reservation.EventTitle,
                         StartDate = reservation.EventStart,
-                        EndDate = reservation.EventEnd
+                        EndDate = reservation.EventEnd,
+                        Person = AppGlobals.currentUser.Id,
+                        PersonName = AppGlobals.currentUser.FullName,
+                        Resource = reservation.Resource.Id,
+                        ResourceName = reservation.Resource.Name,
+                        Category = reservation.Resource.ResourceCategory.Id,
+                        CategoryName = reservation.Resource.ResourceCategory.CategoryName,
                     });
                 }
             }
             model.CountToday = model.TodayReservations.Count;
             model.CountUpcoming = model.UpcomingReservations.Count;
-
+            model.ActiveReservations = model.TodayReservations.Union(model.UpcomingReservations).ToList();
             return model;
         }
     }
