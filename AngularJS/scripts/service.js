c44c0dd1fcf54727389c7cbc824d1fadd07a1fa4
Nedim Čimpo <Nedim impo>
2016-04-04 13:48:17 +0200

    AngularJS Services

---
+++ AngularJS/scripts/service.js
@@ -0,0 +1,43 @@
+﻿(function () {
+
+    var app = angular.module("school");
+
+    app.factory("DataService", function ($http) {
+
+        var source = "http://localhost:55013/api/people/";
+
+        return {
+            list: function () {
+                return $http.get(source)
+            },
+
+            read: function (id) {
+                return $http.get(source + id)
+            },
+
+            create: function (data) {
+                return $http({
+                    method: "post",
+                    url: source,
+                    data: data
+                })
+            },
+
+            update: function (id, data) {
+                return $http({
+                    method: "put",
+                    url: source + id,
+                    data: data
+                })
+            },
+
+            delete: function (set, id) {
+                return $http({
+                    method: "delete",
+                    url: source + id,
+                    data: null
+                })
+            }
+        };
+    });
+}());
