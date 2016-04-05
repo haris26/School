31bd72ad895ed2ed5ead9ae58646aa5d6e5c4643
Nedim Čimpo <Nedim impo>
2016-04-05 14:19:22 +0200

    Sync with dalila

---
+++ AngularJS/scripts/services/skillsService.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module("school");
+
+    app.factory("SkillsService", function ($http) {
+
+        var source = "http://localhost:55013/api/";
+
+        return {
+
+            getCategories: function () {
+                return $http.get(source + "skillscategories")
+            },
+
+
+        };
+    });
+}());
