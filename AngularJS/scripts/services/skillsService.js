61a0b8c602f021a1e618b80ec9bb35d4e1e1de64
dalila-avdukic <dalila@gigischool.com>
2016-04-05 14:49:55 +0200

    skillsService angular added

--- AngularJS/scripts/services/skillsService.js
+++ AngularJS/scripts/services/skillsService.js
@@ -8,10 +8,40 @@
 
         return {
 
-            getCategories: function () {
+            getCategories : function () {
                 return $http.get(source + "skillscategories")
             },
 
+            getCategory: function (id) {
+                return $http.get(source + "skillscategories/" + id)
+            },
+
+            create: function (data, controllerName) {
+                source += controllerName;
+                return $http({
+                    method: "post",
+                    url: source,
+                    data: data
+                })
+            },
+
+            update: function (id, data, controllerName) {
+                source += controllerName +"/";
+                return $http({
+                    method: "put",
+                    url: source + id,
+                    data: data
+                })
+            },
+
+            remove: function (id, controllerName) {
+                source += controllerName + "/";
+                return $http({
+                    method: "delete",
+                    url: source + id,
+                    data: null
+                })
+            }
 
         };
     });
