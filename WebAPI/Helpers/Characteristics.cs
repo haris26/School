65928fa644ec8bd1fa221d2bc8ec0827295b49d3
haris26 <haris.brackovic91@gmail.com>
2016-04-05 16:01:49 +0200

    resource list with angular

---
+++ WebAPI/Helpers/Characteristics.cs
@@ -0,0 +1,32 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+
+namespace WebAPI.Helpers
+{
+    public static class Characteristics
+    {
+        public static IList<CharacteristicsListModel> Create (Resource resource)
+        {
+            SchoolContext context = new SchoolContext();
+            IList<CharacteristicsListModel> models = new List<CharacteristicsListModel>();
+
+            var characteristics = new Repository<Characteristic>(context).Get().ToList().Where(x => x.Resource.Id == resource.Id).ToList();
+
+            foreach(var c in characteristics)
+            {
+                models.Add(new CharacteristicsListModel()
+                {
+                    Id = resource.Id,
+                    Name = c.Name,
+                    Value = c.Value
+                });
+            }
+
+            return models;
+        }
+    }
+}
