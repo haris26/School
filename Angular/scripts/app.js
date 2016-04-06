957e3eabcbf7296747854d259701aeebea36730e
Irma <irmas@gigischool.com>
2016-04-06 15:25:49 +0200

    Modified controller for request representation

---
+++ Angular/scripts/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var app = angular.module("school", ["ngRoute"]);
+
+    app.constant("schConfig",
+        {
+            source: "http://localhost:61310/api/",
+            months: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+            weekdays: ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+            deadline: 5,
+            year: 2016
+        });
+
+    app.config(function ($routeProvider) {
+
+        $routeProvider
+            .when("/requests", { templateUrl: "views/NewRequests.html", controller: "AllRequestsController" })
+            .when("/servicerequests", { templateUrl: "views/ServiceRequests.html", controller: "ServiceRequestsController" })
+            //.when("/roles", { templateUrl: "views/roles.html", controller: "RolesController" })
+            //.when("/engagements", { templateUrl: "views/engagements.html", controller: "EngagementsController" })
+            .otherwise({ redirectTo: "/" });
+    });
+
+}());
---
+++ Angular/scripts/controllers/AllRequestsController.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    var app = angular.module("school");
+
+    app.controller("AllRequestsController", function ($scope, $rootScope, DataService) {
+
+        var dataSet = "newReq";
+        $scope.selString = "";
+        //$scope.sortOrder = "";
+        getRequests();
+
+        fetchData();
+
+        function getRequests() {
+            DataService.list("requests", function (data) {
+                $scope.requests = data.allRequests;
+            });
+        };
+
+        function fetchData() {
+            DataService.list(dataSet, function (data) {
+                $scope.requests= data.allRequests;
+            });
+        }
+
+        $scope.transfer = function (item) {
+            $scope.requests = item;
+    
+        };
+
+      
+
+        
+    });
+
+}());
