edc824da700b23e26c815b017b98c9ecd2c2261a
Martina Vistica <martina@gigischool.com>
2016-04-06 17:03:40 +0200

    cancel event for activeReservations in angularJS

--- Angular/scripts/controllers/userDashboardController.js
+++ Angular/scripts/controllers/userDashboardController.js
@@ -1,23 +1,39 @@
 ﻿(function () {
 
-    var app = angular.module("school", []);
+    var app = angular.module("school");
 
-    app.controller("UserDashboardController", function ($scope, DataService) {
+    app.controller("UserDashboardController", function ($scope, $rootScope, DataService) {
+        var dataSet = "userreservations";
+        fetchData();
 
-      fetchResources();
-
-        function fetchResources() {
-            $scope.message = "Wait...";
-            DataService.getDashboard().then(
-                function (response) {
-                    $scope.dashboard = response.data;
-                    $scope.message = "";
-                },
-                function (reason) {
-                    $scope.message = "No data for that request";
-                }
-            );
+        function fetchData() {
+            DataService.list(dataSet, function (data) {
+                $scope.dashboard = data;
+            });
         }
-      });
 
+        $scope.saveData = function () {
+            var promise;
+            if ($scope.engagement.id == 0) {
+                DataService.create(dataSet, $scope.engagement, function (data) { });
+            }
+            else {
+                DataService.update(dataSet, $scope.engagement.id, $scope.engagement, function (data) { });
+            }
+            fetchData();
+        }
+        function getEvent(id) {
+            DataService.read("events", id , function (data) {
+                $scope.event = data;
+            });
+        };
+
+        $scope.cancelReservation = function(item) {
+            var promise;
+            getEvent(item.id);
+            if ($scope.event != null) {
+                DataService.remove(dataSet, $scope.event.id , function (data) {});
+            }         
+        };
+    });
 }());
