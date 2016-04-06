3fe5eaf24cc53d0267c23be6039b6831a89ce763
haris26 <haris.brackovic91@gmail.com>
2016-04-06 14:34:33 +0200

    resource characteristics modified

--- Angular/scripts/services/resourceService.js
+++ Angular/scripts/services/resourceService.js
@@ -1,43 +1,43 @@
-﻿(function () {
+﻿//(function () {
 
-    var app = angular.module("school");
+//    var app = angular.module("school");
 
-    app.factory("DataService", function ($http) {
+//    app.factory("DataService", function ($http) {
 
-        var source = "http://localhost:50169/api/";
+//        var source = "http://localhost:50169/api/";
 
-        return {
-            resourceList: function () {
-                return $http.get(source + "resources/")
-            },
+//        return {
+//            resourceList: function () {
+//                return $http.get(source + "resources/")
+//            },
 
-            resourceRead: function (id) {
-                return $http.get(source + "resources/" + id)
-            },
+//            resourceRead: function (id) {
+//                return $http.get(source + "resources/" + id)
+//            },
 
-            resourceCreate: function (data) {
-                return $http({
-                    method: "post",
-                    url: source,
-                    data: data
-                })
-            },
+//            resourceCreate: function (data) {
+//                return $http({
+//                    method: "post",
+//                    url: source,
+//                    data: data
+//                })
+//            },
 
-            update: function (id, data) {
-                return $http({
-                    method: "put",
-                    url: source + id,
-                    data: data
-                })
-            },
+//            update: function (id, data) {
+//                return $http({
+//                    method: "put",
+//                    url: source + id,
+//                    data: data
+//                })
+//            },
 
-            delete: function (set, id) {
-                return $http({
-                    method: "delete",
-                    url: source + id,
-                    data: null
-                })
-            }
-        };
-    });
-}());
+//            delete: function (set, id) {
+//                return $http({
+//                    method: "delete",
+//                    url: source + id,
+//                    data: null
+//                })
+//            }
+//        };
+//    });
+//}());
