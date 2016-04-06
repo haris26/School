297392c7102a63c8a062745d5a11bcfd9e8eac41
Nedim Čimpo <Nedim impo>
2016-04-06 11:51:52 +0200

    Skills edit, app.js jquerymin and font added

---
+++ AngularJS/scripts/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module("school", ["ngRoute"]);
+
+    app.constant("schConfig",
+        {
+            source: "http://localhost:55013/api/",
+        });
+
+    app.config(function ($routeProvider) {
+
+        $routeProvider
+            .when("/skills", { templateUrl: "views/skills.html", controller: "SkillsCtrl" })
+            .when("/editCategory", { templateUrl: "views/editCategory.html", controller: "SkillsCtrl" })
+            .otherwise({ redirectTo: "/skills" });
+    });
+
+}());
--- AngularJS/scripts/controllers/skillsController.js
+++ AngularJS/scripts/controllers/skillsController.js
@@ -1,1 +1,25 @@
-﻿
+﻿(function () {
+
+    var app = angular.module("school", []);
+
+    app.controller("SkillsCtrl", function ($scope, SkillsService) {
+
+        $scope.selCategory = "";
+        $scope.sortOrder = "Name";
+        fetchCategories();
+
+        function fetchCategories() {
+            $scope.message = "Wait...";
+            SkillsService.getCategories().then(
+                function (response) {
+                    $scope.categories = response.data;
+                    $scope.message = "";
+                },
+                function (reason) {
+                    $scope.message = "No data for that request";
+                }
+            );
+        }
+    });
+
+}());
