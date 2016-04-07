771e59d121cb9177af77797ac25e2f033797d3b3
lejla-dzeko <lejla@gigischool.com>
2016-04-07 11:20:41 +0200

    merge

---
+++ Angular/scripts/controllers/PeopleController.js
@@ -0,0 +1,48 @@
+(function(){
+
+    var app = angular.module("school");
+
+    app.controller("PeopleController", function($scope, DataService) {
+
+        var dataSet = "people";
+        $scope.selPerson = "";
+        $scope.sortOrder = "lastName";
+        fetchPeople();
+
+        function fetchPeople() {
+            DataService.list("people", function (data) {
+                $scope.people = data;
+            });
+        };
+
+        $scope.transfer = function(item) {
+            $scope.person = item;
+        };
+
+        $scope.newPerson = function() {
+            $scope.person = {
+                id: 0,
+                firstName: "",
+                lastName: "",
+                email: "",
+                phone: "",
+                category: "Full",
+                gender: "",
+                address: {},
+                birthDate: "",
+                startDate: new Date(),
+                status: "Active"
+            }
+        };
+
+        $scope.saveData = function() {
+            if ($scope.person.id == 0){
+                DataService.create(dataSet, $scope.person, function(data){});
+            }
+            else {
+                DataService.update(dataSet, $scope.person.id, $scope.person, function(data){});
+            }
+            fetchPeople();
+        }
+    });
+}());
