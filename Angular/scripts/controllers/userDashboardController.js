6feb9fbcdfc996d96107af06cf11fe1771fcd8a7
Martina Vistica <martina@gigischool.com>
2016-04-07 17:09:21 +0200

    completed userDasboard

--- Angular/scripts/controllers/userDashboardController.js
+++ Angular/scripts/controllers/userDashboardController.js
@@ -2,8 +2,9 @@
 
     var app = angular.module("school");
 
-    app.controller("UserDashboardController", function ($scope, $rootScope, DataService) {
+    app.controller("UserDashboardController", function ($scope, $rootScope, DataService, schConfig) {
         var dataSet = "userreservations";
+        $scope.repeatingTypes = schConfig.repeatingType;
         fetchData();
 
         function fetchData() {
@@ -13,18 +14,33 @@
         }
 
         $scope.cancelReservation = function (item) {
-            var promise;
+            var index = $scope.dashboard.activeReservations.indexOf(item);
             $scope.eventId = item.id;
             getEvent($scope.eventId);
             if ($scope.reservationEvent != undefined) {
                 DataService.remove("events", $scope.reservationEvent.id, function (data) { });
+                $scope.dashboard.activeReservations.splice(index, 1);
             }
+            $scope.$apply();
         };
 
-        //$scope.transfer = function (item) {
-        //    $scope.reservationEvent = item;
-        //};
+        $scope.extendReservation = function (item) {
+            $scope.eventExtend = {
+                id: 0,
+                parentEvent: item.id,
+                repeatUntil: new Date().Date,
+                repeatingType: $scope.repeatingTypes.indexOf(0),
+                frequency: 0
+            }
+            $scope.modalShow = true;
+        };
 
+        $scope.saveData = function () {
+            console.log($scope.eventExtend);
+            DataService.create("eventextends", $scope.eventExtend, function (data) { });
+            $scope.modalShow = false;
+        };
+      
         function getEvent(id) {
             DataService.read("events", id , function (data) {
                 $scope.reservationEvent = data;
