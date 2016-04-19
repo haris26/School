350a6524dbf2dde63220ab1c74d50355d335b16e
nedim <nedim@NEDIM-PC>
2016-04-19 14:23:52 +0200

    qualifications merge

--- AngularJS/scripts/controllers/qualificationsController.js
+++ AngularJS/scripts/controllers/qualificationsController.js
@@ -6,7 +6,13 @@
 
         $scope.message = "Loading data...";
         $scope.sortOrder = "Name";
-        $scope.school = "School";
+
+        $scope.collapsed = {};
+
+        $scope.icon = {
+            true: "glyphicon glyphicon-chevron-down",
+            false: "glyphicon glyphicon-chevron-up"
+        }
 
         fetchEducations();
 
@@ -14,6 +20,9 @@
             DataService.list("educations", function (data) {
                 $scope.educations = data;
                 $scope.message = "";
+                for (i = 0; i < $scope.educations.length; i++) {
+                    $scope.collapsed[$scope.educations[i].type] = true;
+                }
             })
         }
     });
