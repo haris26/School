ddf38eb18cf9df15a65fb73c64bc1e31ab9c2d8e
lejla-dzeko <lejla@gigischool.com>
2016-04-22 09:29:35 +0200

    22.04.

--- Angular/scripts/controllers/Calendar2Controller.js
+++ Angular/scripts/controllers/Calendar2Controller.js
@@ -10,28 +10,12 @@
         fetchData();
         getPeople();
         getDays();
-        //$scope.modalShow = false;
-        //$scope.toggleModal = function () {
-        //    $scope.modalShow = !$scope.modalShow;
-        //};
-        //app.directive('modalDialog', function () {
-        //    return {
-        //        restrict: 'E',
-        //        scope: { show: '=' },
-        //        replace: true,
-        //        transclude: true,
-        //        link: function (scope, element, attrs) {
-        //            scope.dialogStyle = {};
-        //            if (attrs.width) scope.dialogStyle.width = attrs.width;
-        //            if (attrs.height) scope.dialogStyle.height = attrs.height;
-        //            scope.hideModal = function () {
-        //                scope.show = false;
-        //            };
-        //        },
-        //        template: "<div class='modal' ng-show='show'><div class='modal-overlay' ng-click='hideModal()'></div><div class='modal-dialog' ng-style='dialogStyle'><div class='modal-close' ng-click='hideModal()'>X</div><div class='modal-dialog-content' ng-transclude></div></div></div>"
-        //    };
-        //});
-
+        
+        $(document).ready(function () {
+            $('#showmenu').click(function () {
+                $('.menu').slideToggle("fast");
+            });
+        });
 
         function getDays() {
             DataService.read("days", currentUser.id, function (data) {
