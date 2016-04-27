4a7baacdfeac6687c79b389e906ef6f6049ea5c4
irhad-ljubicic <irhad@gigischool.com>
2016-04-27 14:59:22 +0200

    datePicker

---
+++ Angular/scripts/services/DatePicker.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module("school");
+
+    app.directive("datePicker", function ($modal, schConfig) {
+        return {
+            restrict: "AE",
+            replace:true,
+            template:'<div>{{today}}</div>',
+      
+            link: function (scope, elem, attrs) {
+                scope.today = new Date(1991, 2, 2);
+                console.log(scope.today);
+            }
+            
+        };
+    });
+}());
