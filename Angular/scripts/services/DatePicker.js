fcaab8970c6534505fceba7bbd1072dd580af9e5
irhad-ljubicic <irhad@gigischool.com>
2016-04-29 11:20:52 +0200

    date picker

--- Angular/scripts/services/DatePicker.js
+++ Angular/scripts/services/DatePicker.js
@@ -6,11 +6,42 @@
         return {
             restrict: "AE",
             replace:true,
-            template:'<div>{{today}}</div>',
-      
+            templateUrl:'views/datePicker.html',
+            scope: {
+                today: '=',
+                week: '=',
+            },
             link: function (scope, elem, attrs) {
-                scope.today = new Date(1991, 2, 2);
-                console.log(scope.today);
+                
+                var setToday = new Date();
+                scope.today = setToday.setUTCHours(-2);
+              
+
+                scope.week = {
+                    start: new Date(setToday.getFullYear(),setToday.getMonth(),(setToday.getDate()-setToday.getDay()+2)),
+                    end: new Date(setToday.getFullYear(),setToday.getMonth(),(setToday.getDate()-setToday.getDay()+6))
+                };
+                
+
+            scope.next=function () {
+                     scope.week = {
+                         start: new Date(scope.week.start.getFullYear(), scope.week.start.getMonth(), scope.week.start.getDate() +6),
+                         end: new Date(scope.week.end.getFullYear(), scope.week.end.getMonth(), scope.week.end.getDate() +6),
+                     };
+                     
+                     scope.$parent.getWeek(scope.week);
+                     
+                }
+          
+      
+               scope.previous = function () {
+                   scope.week = {
+                       start: new Date(scope.week.start.getFullYear(), scope.week.start.getMonth(), scope.week.start.getDate() - 7),
+                       end: new Date(scope.week.end.getFullYear(), scope.week.end.getMonth(), scope.week.end.getDate() - 7),
+                       
+                   };
+                   scope.$parent.getWeek(scope.week);
+               }
             }
             
         };
