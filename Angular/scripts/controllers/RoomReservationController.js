a360132e904cfdb45ef46c485a4b59a530929119
irhad-ljubicic <irhad@gigischool.com>
2016-04-29 16:59:53 +0200

    room reservations

---
+++ Angular/scripts/controllers/RoomReservationController.js
@@ -0,0 +1,30 @@
+﻿(function () {
+
+    var app = angular.module("school");
+
+    app.controller("RoomReservationController", function ($scope, $rootScope, DataService) {
+
+        $scope.searchParameters = {
+            fromDate: new Date(),
+            toDate: new Date(),
+            categoryName: "Room",
+            resourceName: "",
+            osType: ""
+        };
+        
+       function getReservation() {
+            DataService.create("reservationoverview", $scope.searchParameters, function (data) {
+                $scope.reservations = data;
+                console.log($scope.reservations);
+            });
+
+       }
+       getReservation();
+
+        $rootScope.refreshTable = function () {
+            $scope.getReservations();
+        }
+        
+    });
+}());
+
